7f8f20b676d27697e6d72dc0da37dc343235117e
Rachmad Suharja <rachmadsuharja123@gmail.com>
2024-10-19 09:56:23 +0700

    easter egg

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,8 @@ function play() {
   const audio = document.getElementById("music");
   audio.play();
 }
+
+function firstIlustration() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = "block";
+}
